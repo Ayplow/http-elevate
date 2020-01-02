662953f31701181305268da352b3febec7a820ad
Tom Sputz <Tom Sputz>
2020-01-02 02:40:29 +0000

    feat: support `init` argument

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 addEventListener('fetch', event =>
-  event.respondWith(
-    event.request
-      .json()
-      .then(fetch)))
+  event.respondWith(handleRequest(event.request)),
+)
+
+async function handleRequest(request) {
+  const { searchParams } = new URL(request.url)
+  const init = searchParams.has('init')
+    ? JSON.parse(searchParams.get('init'))
+    : await request.json()
+  const resource = searchParams.get('resource')
+
+  return fetch(resource, init)
+}
